a01cf02dc0d646ea216b73ee721f357c7354d4be
KaitouKiddo <sherrylavender0314@gmail.com>
2020-10-29 23:28:22 -0400

    added fonts and theme colors

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import { connect } from "react-redux";
+import Helmet from 'react-helmet';
 
+import DateFnsUtils from "@date-io/date-fns";
+import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import { StylesProvider } from "@material-ui/styles";
+import { ThemeProvider } from "styled-components";
+
+import maTheme from "./theme";
 import Routes from "./routes/Routes";
 
-function App() {
+function App({ theme }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Routes />
-      </header>
-    </div>
+    <React.Fragment>
+      <Helmet
+        titleTemplate="%s | Material App"
+        defaultTitle="Material App - React Admin & Dashboard Template"
+      />
+      <StylesProvider injectFirst>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <MuiThemeProvider theme={maTheme[theme.currentTheme]}>
+            <ThemeProvider theme={maTheme[theme.currentTheme]}>
+              <Routes />
+            </ThemeProvider>
+          </MuiThemeProvider>
+        </MuiPickersUtilsProvider>
+      </StylesProvider>
+    </React.Fragment>
   );
 }
 
